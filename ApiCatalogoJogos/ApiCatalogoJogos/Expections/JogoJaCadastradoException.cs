ed18c0e6bf8c5e78b68fa1e29dc69bd1fda832bb
Mateus Lopes <mateusfellipe2302@gmail.com>
2021-07-08 19:09:09 -0300

    Add Api Catalogo de Jogos

---
+++ ApiCatalogoJogos/ApiCatalogoJogos/Expections/JogoJaCadastradoException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ApiCatalogoJogos.Expections
+{
+    public class JogoJaCadastradoException : Exception
+    {
+        public JogoJaCadastradoException()
+            : base("Este jogo já está cadastrado") 
+        { }
+    }
+}
---
+++ ApiCatalogoJogos/ApiCatalogoJogos/Services/JogoService.cs
@@ -0,0 +1,117 @@
+﻿using ApiCatalogoJogos.Entities;
+using ApiCatalogoJogos.Expections;
+using ApiCatalogoJogos.InputModel;
+using ApiCatalogoJogos.Repositories;
+using ApiCatalogoJogos.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Services
+{
+    public class JogoService : IJogoService
+    {
+        private readonly IJogoRepository _jogoReposiroty;
+
+        public JogoService(IJogoRepository jogoReposiroty)
+        {
+            _jogoReposiroty = jogoReposiroty;
+        }
+
+        public async Task<List<JogoViewModel>> Obter(int pagina, int quantidade)
+        {
+            var jogos = await _jogoReposiroty.Obter(pagina, quantidade);
+
+            return jogos.Select(jogo => new JogoViewModel
+            {
+                Id = jogo.Id,
+                Nome = jogo.Nome,
+                Produtora = jogo.Produtora,
+                Preco = jogo.Preco
+            }).ToList();
+        }
+
+        public async Task<JogoViewModel> Obter(Guid id)
+        {
+            var jogo = await _jogoReposiroty.Obter(id);
+
+            if (jogo == null)
+                return null;
+
+            return new JogoViewModel
+            {
+                Id = jogo.Id,
+                Nome = jogo.Nome,
+                Produtora = jogo.Produtora,
+                Preco = jogo.Preco
+            };
+        }
+
+        public async Task<JogoViewModel> Inserir(JogoInputModel jogo)
+        {
+            var entidadeJogo = await _jogoReposiroty.Obter(jogo.Nome, jogo.Produtora);
+
+            if (entidadeJogo.Count > 0)
+                throw new JogoJaCadastradoException();
+
+            var jogoInsert = new Jogo
+            {
+                Id = Guid.NewGuid(),
+                Nome = jogo.Nome,
+                Produtora = jogo.Produtora,
+                Preco = jogo.Preco
+            };
+
+            await _jogoReposiroty.Inserir(jogoInsert);
+
+            return new JogoViewModel
+            {
+                Id = jogoInsert.Id,
+                Nome = jogoInsert.Nome,
+                Produtora = jogo.Produtora,
+                Preco = jogo.Preco
+            };
+        }
+
+        public async Task Atualizar(Guid id, JogoInputModel jogo)
+        {
+            var entidadeJogo = await _jogoReposiroty.Obter(id);
+
+            if (entidadeJogo == null)
+                throw new JogoNaoCadastradoException();
+
+            entidadeJogo.Nome = jogo.Nome;
+            entidadeJogo.Produtora = jogo.Produtora;
+            entidadeJogo.Preco = jogo.Preco;
+
+            await _jogoReposiroty.Atualizar(entidadeJogo);
+        }
+
+        public async Task Atualizar(Guid id, double preco)
+        {
+            var entidadeJogo = await _jogoReposiroty.Obter(id);
+
+            if (entidadeJogo == null)
+                throw new JogoNaoCadastradoException();
+
+            entidadeJogo.Preco = preco;
+
+            await _jogoReposiroty.Atualizar(entidadeJogo);
+        }
+        public async Task Remover(Guid id)
+        {
+            var jogo = await _jogoReposiroty.Obter(id);
+
+            if (jogo == null)
+                throw new JogoNaoCadastradoException();
+
+            await _jogoReposiroty.Remover(id);
+        }
+
+        public void Dispose()
+        {
+            _jogoReposiroty?.Dispose();
+        }
+    }
+}
